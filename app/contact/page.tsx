ac3f5caec71bdb12ffab917d46164c841a2a9d64
iftekhar2979 <salminifti79@gmail.com>
2024-11-26 15:51:09 +0600

    Changes on title

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,14 @@
 // import Banner from '@/components/contact/Banner'
+import type { Metadata } from "next";
 import ContactBanner from '@/components/contact/ContactBanner'
 import ContactInfo from '@/components/contact/ContactInfo'
 import StaticMap from '@/components/contact/LocationMap'
 import { ContactUs } from '@/components/ContactUs/ContactUs'
 import React from 'react'
-
+export const metadata: Metadata = {
+  title: "Contact Aerotech Aircraft Maintenance Ltd.",
+  description: "Contact Aerotech Aircraft Maintenance Ltd.",
+};
 export default function page() {
   return (
     <div>
